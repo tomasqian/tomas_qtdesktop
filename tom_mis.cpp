7b6ed5ff6e6a4afbf04aa95ff98acda5dec3ee15
tomasqian <tomas_qian@hotmail.com>
2018-05-04 23:37:26 +0800

    destop first committ

---
+++ tom_mis.cpp
@@ -0,0 +1,14 @@
+#include "tom_mis.h"
+#include "ui_tom_mis.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ tom_mis.h
@@ -0,0 +1,22 @@
+#ifndef TOM_MIS_H
+#define TOM_MIS_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // TOM_MIS_H
